5b7ee8ff1abe11b88c87461142683a6d015eebd0
miusov <miusov86@gmail.com>
2018-04-12 22:47:41 +0300

    add saving repositories to disc

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
+ini_set('max_execution_time', 600);
 header('Content-Type: text/html; charset=utf-8');
 require 'lib/phpQuery.php';
 
-$url        = 'https://bitbucket.org/dashboard/repositories';
+$url       = 'https://bitbucket.org/dashboard/repositories';
 $auth_data = [
-    'username'  => '*********',
-    'password'  => '******'
+    'username'  => '*****',
+    'password'  => '*****'
 ];
 
 function getContent($url,$auth=[])
@@ -26,9 +27,8 @@ function getContent($url,$auth=[])
     return $res;
 }
 
-
-
-function parseToArray($url,$auth_data,$start,$end)
+//парсим ссылки и сохраняем в data.txt
+function parseToTxt($url,$auth_data,$start,$end)
 {
     if ($start<$end)
     {
@@ -38,21 +38,85 @@ function parseToArray($url,$auth_data,$start,$end)
         {
             $item = pq($item);
             $fp = fopen('data.txt', 'a');
-            fwrite($fp, $item->attr('href') . PHP_EOL);
+            fwrite($fp, 'https://bitbucket.org'.$item->attr('href').'/downloads/' . PHP_EOL);
             fclose($fp);
+            echo "<code>parse ...".$item->attr('href')."</code><br>";
         }
         $next = $doc->find('ol.aui-nav-pagination li.aui-nav-selected')->next()->find('a#page-number-link')->attr('href');
         if (!empty($next))
         {
             $start++;
-            parseToArray('https://bitbucket.org'.$next,$auth_data,$start,$end);
+            parseToTxt('https://bitbucket.org'.$next,$auth_data,$start,$end);
         }
-        echo $next.' - SUCCESSFULLY<br>';
     }
-
     return true;
 }
 
+//сохраняем репозитории в result
+function parseArchive($auth_data)
+{
+    $urls = file('data.txt');
+    if (!empty($urls))
+    {
+        foreach ($urls as $url)
+        {
+            $url = rtrim($url);
+            $data = getContent($url,$auth_data);
+            $doc = phpQuery::newDocument($data);
+            $name = $doc->find('ol.aui-nav-breadcrumbs li:last-child a')->text();
+            $name = str_replace(' ','_',$name);
+            $href = $doc->find('table#uploaded-files tr.download-repo a.lfs-warn-link')->attr('href');
+            $href = 'https://bitbucket.org'.$href;
+            $path = $_SERVER['DOCUMENT_ROOT'] . '/result/'.$name.'.zip';
+            curl_download($href,$path,$auth_data);
+            echo "<code><i>Load ".$name."</i></code><br>";
+        }
+    }else{
+        echo 'EMPTY data.txt!';
+    }
+
+}
+
+function curl_download($url, $file, $auth)
+{
+    // открываем файл, на сервере, на запись
+    $dest_file = @fopen($file, "w");
+    // открываем cURL-сессию
+    $resource = curl_init();
+    // устанавливаем опцию удаленного файла
+    curl_setopt($resource, CURLOPT_URL, $url);
+    // авторизируемся на сайте
+    curl_setopt($resource, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($resource, CURLOPT_USERPWD, $auth['username'] . ":" . $auth['password']);
+    curl_setopt($resource, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($resource, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($resource, CURLOPT_COOKIEJAR, __DIR__.'/cookie.txt');
+    curl_setopt($resource, CURLOPT_COOKIEFILE, __DIR__.'/cookie.txt');
+    curl_setopt($resource, CURLOPT_TIMEOUT, 30);
+    // устанавливаем место на сервере, куда будет скопирован удаленной файл
+    curl_setopt($resource, CURLOPT_FILE, $dest_file);
+    // заголовки нам не нужны
+    curl_setopt($resource, CURLOPT_HEADER, 0);
+    // выполняем операцию
+    curl_exec($resource);
+    // закрываем cURL-сессию
+    curl_close($resource);
+    // закрываем файл
+    fclose($dest_file);
+}
+
+//----------------------------------------------------------------------------------------
+//количество станиц пагинации
 $start = 0;
 $end = 5;
-parseToArray($url,$auth_data,$start,$end);
+
+//получаем и записываем ссылки в data.txt
+echo "<code><strong>Start...</strong></code> <br>";
+parseToTxt($url,$auth_data,$start,$end);
+echo "<code><strong>End parse url's</strong></code> <br><hr><br>";
+
+//скачиваем репозитории
+echo "<code><strong>Start download repositories...</strong></code> <br>";
+//parseArchive($auth_data);
+echo "<code><strong>END.</strong></code> <br>";
+//----------------------------------------------------------------------------------------
